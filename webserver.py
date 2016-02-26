21a951d656804f2097ef4b607577dd17a863be40
Waregalias <said.mezhoud@voidmx.net>
2016-02-26 12:40:09 +0100

    thread start success

--- webserver.py
+++ webserver.py
@@ -9,40 +9,50 @@ import string
 import threading
 import time
 
-colorAuto = 0
+global select_mode
+global colorAuto
 
 def rainbow():
     print("Thread started!")
-    sleep(.1)
+    #url = 'http://10.13.9.37/arduino/manual/100/0/0'
+    #response = urllib2.urlopen(url).read()
+            
+    #self.send_response(200)
+    #self.end_headers()
+    #self.wfile.write(response)
 
 class GetHandler(BaseHTTPRequestHandler):
     def select_MODE(self):
+        select_mode = 0
+        color_auto = 0
         parsed_path = urlparse.urlparse(self.path)
         choice = self.path
         cleaned = choice.split("/")
 
-        if cleaned[1] == 'auto' and cleaned[2] == 1 :
+        if cleaned[1] == 'auto' and cleaned[2] == '1' :
             colorAuto = threading.Thread(target=rainbow)
             colorAuto.start()
             
-        if cleaned[1] == 'auto' and cleaned[2] == 0 :
+        if cleaned[1] == 'auto' and cleaned[2] == '0' :
             colorAuto = threading.Thread(target=rainbow)
             colorAuto.stop()
             
         if cleaned[1] == 'manual' :
             select_mode = 'color/' + cleaned[2] + '/' + cleaned[3] + '/' + cleaned[4]
+            print "manual"
 
         return select_mode
 
     def do_GET(self):
         mode = self.select_MODE()
-        url = 'http://10.13.9.37/arduino/' + mode
-        response = urllib2.urlopen(url).read()
-        
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(response)
 
+        if mode != 0:
+            url = 'http://10.13.9.37/arduino/' + mode
+            response = urllib2.urlopen(url).read()
+            
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(response)
         return
 
 if __name__ == '__main__':
